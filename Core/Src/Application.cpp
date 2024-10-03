6e3eb17ce57777dfd4c91fc0e2c1c3d58f076b53
CamModreski <cameronmodreski@gmail.com>
2024-10-03 18:55:44 -0400

    Stubs for Application Class and Semaphore Class

---
+++ Core/Src/Application.cpp
@@ -0,0 +1,13 @@
+/*
+ * Application.cpp
+ *
+ *  Created on: Oct 3, 2024
+ *      Author: camca
+ */
+
+#include "Application.h"
+
+Application::Application(int32_t shape, int32_t freq, int32_t ampl) {
+
+}
+
---
+++ Core/Src/Application.h
@@ -0,0 +1,17 @@
+/*
+ * Application.h
+ *
+ *  Created on: Oct 3, 2024
+ *      Author: camca
+ */
+
+#ifndef SRC_APPLICATION_H_
+#define SRC_APPLICATION_H_
+
+class Application {
+public:
+	Application();
+	virtual ~Application();
+};
+
+#endif /* SRC_APPLICATION_H_ */
