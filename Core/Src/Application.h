1154a7cfed5f336947809dcade503da8ae35e3b9
CamModreski <cameronmodreski@gmail.com>
2024-10-08 14:22:28 -0400

    Application Class

---
+++ Core/Src/Application.h
@@ -0,0 +1,13 @@
+/*
+ * Application.h
+ *
+ *  Created on: Oct 3, 2024
+ *      Author: camca
+ */
+
+class Application {
+public:
+	Application();
+	
+};
+
---
+++ Core/Src/IPCsemaphore.h
@@ -0,0 +1,12 @@
+/*
+ * IPCsemaphore.h
+ *
+ *  Created on: Oct 3, 2024
+ *      Author: camca
+ */
+
+
+class IPCsemaphore {
+public:
+	IPCsemaphore();
+};
